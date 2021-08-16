6bad8325a1eb3e88102a35c2f7f1af505c3f8ad1
Lorelei <lorelei.miyamura@gmail.com>
2021-08-15 21:26:37 -0700

    remove template comments

--- acf-post-slug.php
+++ acf-post-slug.php
@@ -22,20 +22,6 @@ class NAMESPACE_acf_plugin_FIELD_NAME {
 	// vars
 	var $settings;
 	
-	
-	/*
-	*  __construct
-	*
-	*  This function will setup the class functionality
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	void
-	*  @return	void
-	*/
-	
 	function __construct() {
 		
 		// settings
@@ -51,20 +37,6 @@ class NAMESPACE_acf_plugin_FIELD_NAME {
 		add_action('acf/include_field_types', 	array($this, 'include_field')); // v5
 		add_action('acf/register_fields', 		array($this, 'include_field')); // v4
 	}
-	
-	
-	/*
-	*  include_field
-	*
-	*  This function will include the field type class
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	$version (int) major ACF version. Defaults to false
-	*  @return	void
-	*/
 	
 	function include_field( $version = false ) {
 		
